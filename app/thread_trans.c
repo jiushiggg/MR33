892a8791694967cea32dcafe0928894edc338e23
gaolongfei <longfei.gao@hanshow.com>
2020-10-12 18:27:35 +0800

    add

--- app/thread_trans.c
+++ app/thread_trans.c
@@ -20,10 +20,13 @@
 #include <xdc/runtime/Error.h>
 
 #include "thread_trans.h"
+#include "memery.h"
 #include "debug.h"
 #include "event.h"
 #include "bsp_spi.h"
 #include "bsp_uart.h"
+#include "crc16.h"
+#include "cc2640r2_rf.h"
 
 #define ABORT_ID    0x1000
 
@@ -52,20 +55,24 @@ typedef struct trans_struct {
 
 
 static void thread_transmit_init(void);
+static void trans_downlink_handle(uart_tsk_msg_t* msg);
+static void trans_uplink_handle(uart_tsk_msg_t* msg);
 
+#define UART_BUF_NUM    2
 List_List trans_list;
-trans_struct trans_buf[2];
-uint8_t buffer[2][BUFFER_LEN];
+trans_struct trans_buf[UART_BUF_NUM];
+uint8_t buffer[UART_BUF_NUM][BUFFER_LEN];
 
 Mailbox_Handle trans_mbox;
 extern Mailbox_Handle rf_mbox;
 
 #define UART_HEAD   (uint16_t)24
-#define UART_MAX_DATA   (uint16_t)512
-#define UART_BUF_LEN    (UART_HEAD+UART_MAX_DATA)
-uint8_t uart_txbuf[UART_BUF_LEN];                  // send buffer
+#define UART_MAX_LEN   (uint16_t)512
+#define UART_BUF_LEN    (UART_HEAD+UART_MAX_LEN)
+
 uint8_t uart_rxbuf[UART_BUF_LEN];               // Receive  buffer
 static uint8_t uart_sync[4] = {0xf0, 0xf0, 0xf0, 0xf0};
+int16_t rf_ch;
 
 void *thread_transmit(UArg arg)
 {
@@ -127,103 +134,171 @@ typedef enum _up_handle{
 }up_handle;
 
 typedef enum _down_handle{
-    DOWNLINK_START,
+    DOWNLINK_START  = (uint8_t)1,
     DOWNLINK_JUDGE,
-    DOWNLINK_ACK,
+    DOWNLINK_PACKAGE,
+    DOWNLINK_SEND_ACK,
     DOWNLINK_ERR,
     DOWNLINK_END
 }down_handle;
 
 void trans_uplink_handle(uart_tsk_msg_t* msg)
 {
+#if 0
     static up_handle u_status = UPLINK_START;
+    static uart_head_st* head_addr  = NULL;
+    static uart_head_st* data_addr  = NULL;
+    static uart_head_st* tx_head_addr = NULL;
+    uint8_t i, exit_flg = 1;
 
-
-    switch(u_status){
-        case UPLINK_START:
-            task_id = head->idx;
-            if (trans_buf.total)
-
-            break;
-        case :
-            break;
-        default:
-            break;
-    }
-}
-
-void trans_downlink_handle(uart_tsk_msg_t* msg)
-{
-    static down_handle d_status = DOWNLINK_START;
-    uart_head_st* msg_head  = (uart_head_st*)msg.buf;
-    uart_head_st* head_addr  = NULL;
-    uart_head_st* data_addr = NULL;
-    uint8_t i = 0;
-    uint16_t crc = 0;
-    static uint16_t task_id = 0;
-    while(){
-        switch(d_status){
-            case DOWNLINK_START:
-                task_id = head_addr->idx;
-                head_addr = ap_malloc(sizeof(uart_head_st));
-                memcpy(head_addr, msg_head, sizeof(uart_head_st));
-
-                for(i=0; i<2; i++){
-                    if (TRANS_BUF_IDLE == trans_buf[i].buf_status){
-                        break;
-                    }
-                }
-                if (i == 2){
-                    data_addr = ap_malloc(head_addr->len);
-                }else {
-                    data_addr = trans_buf[i].buf;
-                }
-                d_status = DOWNLINK_JUDGE;
-                //no break;
-            case DOWNLINK_JUDGE:
-                if (0 != memcmp(head_addr->sync, uart_sync, sizeof(uart_sync))){
-                    goto downlink_ack;
-                }
-                if (head_addr->len > UART_MAX_DATA){
-                    goto downlink_ack;
-                }
-                crc = CRC16_CaculateStepByStep();
-                if (crc != head_addr->crc){
-                    goto downlink_ack;
-                }
-                if (task_id == ABORT_ID){
-                    //todo: send message to rf thread, stop current task
-                    //then rf send message to transmit thread
-                    break;
-                }
-                if (task_id != head_addr->id){
-                    goto downlink_ack;
-                }
-
-                //break;
-            case DOWNLINK_ACK:
-
-                if (head_addr->pkg == head_addr->idx){
-                    d_status = DOWNLINK_END;
-                }
+    while(exit_flg){
+        switch(u_status){
+            case UPLINK_START:
                 break;
-            case  DOWNLINK_END:
-                ap_free(head_addr);
-                ap_free(data_addr);
-                d_status = DOWNLINK_START;
+            case UPLINK_TRNAS:
                 break;
             default:
                 break;
         }
     }
+#endif
+}
 
+void trans_downlink_handle(uart_tsk_msg_t* msg)
+{
+    static down_handle d_status = DOWNLINK_START;
+    static uart_head_st* head_addr  = NULL;
+    static uart_head_st* data_addr  = NULL;
+    static uart_head_st* tx_head_addr = NULL;
+    uart_head_st* msg_head  = (uart_head_st*)msg->buf;
+    uint8_t exit_flg = 1;
+    static uint16_t task_id = 0;
+    static uint8_t i;
+
+    while(exit_flg){
+        switch(d_status){
+            case DOWNLINK_START:
+                task_id = msg_head->idx;
+                head_addr = ap_malloc(sizeof(uart_head_st));
+                tx_head_addr = ap_malloc(sizeof(uart_head_st));
+
+                for(i=0; i<UART_BUF_NUM; i++){
+                    if (TRANS_BUF_IDLE == trans_buf[i].buf_status){
+                        break;
+                    }
+                }
+                if (i == UART_BUF_NUM){
+                    data_addr = ap_malloc(UART_MAX_LEN);
+                }else {
+                    trans_buf[i].buf_status = TRANS_BUF_USING;
+                    data_addr = (uart_head_st*)trans_buf[i].buf;
+                }
+                d_status = DOWNLINK_JUDGE;
+                break;
+            case DOWNLINK_JUDGE:
+            {
+                uint16_t crc = 0;
+
+                memcpy(head_addr, msg_head, sizeof(uart_head_st));
+                memcpy(data_addr, msg_head->data, head_addr->len);
+
+                if (0 != memcmp(head_addr->sync, uart_sync, sizeof(uart_sync))){
+                    tx_head_addr->ctrl = CTRL_ERR_HEAD;
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                if (head_addr->len > UART_MAX_LEN){
+                    tx_head_addr->ctrl = CTRL_ERR_LEN;
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                crc = CRC16_CaculateStepByStep(0, (uint8_t*)head_addr, offsetof(uart_head_st, crc));
+                crc = CRC16_CaculateStepByStep(crc, (uint8_t*)data_addr, head_addr->len);
+                if (crc != head_addr->crc){
+                    tx_head_addr->ctrl = CTRL_ERR_CRC;
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                if (head_addr->id == ABORT_ID){
+                    RF_cancle(rf_ch);
+                    //todo: send message to rf thread, stop current rf task
+                    //then rf send message to transmit thread
+                    exit_flg = 0;
+                    d_status = DOWNLINK_SEND_ACK;
+                    break;
+                }
+                if (task_id != head_addr->id){
+                    tx_head_addr->ctrl = CTRL_ERR_ID;
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                if (UART_BUF_NUM == i){
+                    //todo: if return no buffer, need a event, check buffer, then send message to elinker
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                if ((uint8_t*)data_addr+head_addr->len > trans_buf[i].buf){
+                    tx_head_addr->ctrl = CTRL_OVER_FLOW;
+                    d_status = DOWNLINK_PACKAGE;
+                    break;
+                }
+                data_addr += head_addr->len;
+                d_status = DOWNLINK_PACKAGE;
+                break;
+            }
+            case DOWNLINK_PACKAGE:
+                memcpy(tx_head_addr, uart_sync, sizeof(uart_sync));
+                tx_head_addr->tx_sn++;
+                tx_head_addr->rx_sn = head_addr->tx_sn;
+                tx_head_addr->ctrl |= CTRL_ACK;
+                if (UART_BUF_NUM == i){
+                    tx_head_addr->win = (&trans_buf[i].buf[BUFFER_LEN]-(uint8_t*)data_addr)/UART_MAX_LEN;
+                } else {
+                    tx_head_addr->win = 0;
+                }
+                tx_head_addr->id = head_addr->id;
+                tx_head_addr->ack_req = 0;
+                tx_head_addr->next_id = 0;
+                tx_head_addr->len = 0;
+                tx_head_addr->crc = CRC16_CaculateStepByStep(0, (uint8_t*)tx_head_addr, offsetof(uart_head_st, crc));
+                //todo: add send dongle log
+                d_status = DOWNLINK_SEND_ACK;
+                break;
+            case DOWNLINK_SEND_ACK:
+            {
+                uart_write_pend(EVENT_WAIT_FOREVER);
+                bsp_uart_write(tx_head_addr, sizeof(uart_head_st));
+
+                if (head_addr->pkg == head_addr->idx && tx_head_addr->ctrl==CTRL_ACK){
+                    d_status = DOWNLINK_END;
+                }else {
+                    d_status = DOWNLINK_JUDGE;
+                }
+                break;
+            }
+            case  DOWNLINK_END:
+                ap_free(head_addr, sizeof(uart_head_st));
+                head_addr = NULL;
+                ap_free(data_addr, UART_MAX_LEN);
+                data_addr = NULL;
+                ap_free(tx_head_addr, sizeof(uart_head_st));
+                tx_head_addr = NULL;
+                exit_flg = 0;
+                d_status = DOWNLINK_START;
+                break;
+            case  DOWNLINK_ERR: //no break;
+            default:
+                //todo:error handle
+                break;
+        }
+    }
 }
 
 static List_Elem* trans_list_init(void)
 {
     trans_buf[0].buf = buffer[0];
     trans_buf[1].buf = buffer[1];
-    trans_buf[0].buf_total = trans_buf[1].buf_total = BUFFER_LEN/UART_MAX_DATA;
+    trans_buf[0].buf_total = trans_buf[1].buf_total = BUFFER_LEN/UART_MAX_LEN;
     trans_buf[0].buf_index = trans_buf[1].buf_index = 0;
     trans_buf[0].buf_status = trans_buf[1].buf_status = TRANS_BUF_IDLE;
 
--- app/thread_trans.h
+++ app/thread_trans.h
@@ -10,8 +10,13 @@
 
 
 typedef enum _uart_head_ctrl{
-    CTRL_ACK = 0,
-    CTRL_MSG,
+    CTRL_ACK = (uint8_t)1,
+    CTRL_MSG = (uint8_t)2,
+    CTRL_ERR_LEN = (uint8_t)0x20,
+    CTRL_ERR_CRC = (uint8_t)0x40,
+    CTRL_ERR_ID =  (uint8_t)0x80,
+    CTRL_ERR_HEAD = (uint8_t)0x60,
+    CTRL_OVER_FLOW = (uint8_t)0xC0,
 }uart_head_ctrl;
 
 typedef struct _uart_head_st{
@@ -20,24 +25,37 @@ typedef struct _uart_head_st{
     uint16_t rx_sn;
     uart_head_ctrl ctrl;
     uint8_t win;
-    uint8_t id;
+    uint16_t id;
     uint16_t pkg;
     uint16_t idx;
-    uint32_t ret_code;
+    uint16_t ack_req;
+    uint16_t next_id;
     uint16_t len;
     uint16_t crc;
     uint8_t data[0];
 }uart_head_st;
 
-typedef enum _msg_type{
-    MSG_UPLINK = 1,
-    MSG_UART_CB = 1,
+typedef enum _em_msg_type{
+    MSG_UPLINK = (uint8_t)1,
+    MSG_UART_CB,
     MSG_TRANS_ACK,
     MSG_ERR
-}msg_type;
+}em_msg_type;
+
+typedef enum _em_rf_task{
+    RF_UPDATE = (uint8_t)1,
+    RF_HB,
+    RF_BG_SCAN,
+    RF_AP_LOCATION,
+    RF_SURVEY,          //5
+    RF_HB_BG,
+    RF_UPLINK,
+    RF_IDLE,
+}em_rf_task;
 
 typedef  struct _uart_tsk_msg_t{
-    msg_type type;
+    em_msg_type type;
+    em_rf_task rf_task;
     uint16_t id;
     uint32_t len;
     uint8_t* buf;
